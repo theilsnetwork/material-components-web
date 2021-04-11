3384d524e25491fdf3b69e94f0106ba7654ddee5
Liz Mitchell <lizmitchell@google.com>
2021-04-10 22:01:57 -0700

    fix(rtl): mixins work with pseudo elements
    PiperOrigin-RevId: 367840417

---
+++ packages/mdc-rtl/test/rtl.scss.test.ts
@@ -0,0 +1,56 @@
+/**
+ * @license
+ * Copyright 2020 Google Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import * as fs from 'fs';
+import * as path from 'path';
+
+describe('rtl.test.scss', () => {
+  let css = '';
+
+  beforeAll(() => {
+    const filePath = path.join(__dirname, 'rtl.test.css');
+    css = fs.readFileSync(filePath, 'utf8').trim();
+  });
+
+  it('should not add [dir=rtl] after pseudo element', () => {
+    expect(css).not.toContain('.test-pseudo-element::before[dir=rtl]');
+    expect(css).toContain('.test-pseudo-element[dir=rtl]::before');
+  });
+
+  it('should not add [dir=rtl] to child BEM elements', () => {
+    expect(css).not.toContain('.test-bem__child-no-rtl-append[dir=rtl]');
+    expect(css).toContain('[dir=rtl] .test-bem__child-no-rtl-append');
+  });
+
+  it('should add [dir=rtl] if last child in complex selector is not element BEM class',
+     () => {
+       expect(css).toContain(
+           '.test-bem__child .test-bem--should-have-rtl[dir=rtl]');
+     });
+
+  it('should add [dir=rtl] to parent if child element BEM has a superselector',
+     () => {
+       expect(css).toContain('.test-bem[dir=rtl] .test-bem__child');
+       expect(css).toContain('[dir=rtl] .test-bem .test-bem__child');
+     });
+});
